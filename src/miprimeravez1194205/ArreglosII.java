34830e33eddd83604c6586e0ce3997a6fc65d8ac
PERSONAL <PERSONAL@10.1.18.101>
2016-11-12 09:00:54 -0800

    llenado de los arreglos y calculo

--- src/miprimeravez1194205/ArreglosII.java
+++ src/miprimeravez1194205/ArreglosII.java
@@ -9,12 +9,16 @@ public class ArreglosII {
         Scanner read = new Scanner (System.in);
         System.out.println("Ingrese cantidad de usuarios");
         int largo = read.nextInt();
-        String datos [] = new String [largo];
+        String[] nombre = new String [largo];
+        int[]nacimiento=new int[largo];
+        byte[]edad=new byte[largo];
         
-        for (int i = 0; i < datos.length; i++) {
-            System.out.println("Ingrese el dato para el usuario"+ (i+1));
-            datos[i] = read.nextLine();
-            
+        for (int i = 0; i < nombre.length; i++) {
+            System.out.println("Ingrese el dato para el usuario "+ (i+1));
+            nombre[i] = read.nextLine();
+            System.out.println("Ingrese el año de nacimiento para el usuario "+ (i+1));
+            nacimiento[i]=read.nextInt();
+            edad[i]=(byte)(2016-nacimiento[i]);
         }
     }
     
